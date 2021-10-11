999be4e0af3c4e022ef257574763dba3b4a1221d
Olha Tsyhanenko <olya.tsyganenko@gmail.com>
2021-10-11 22:26:45 +0300

    добавила модалку, закрытие по Escape и нажатии на Overley

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import { Component } from 'react';
 import './App.css';
+import Modal from './components/Modal/Modal';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+  state = {
+        showModal: false,
+    };
+
+    toggleModal = () => {
+        this.setState(state => ({
+            showModal: !state.showModal
+        }))
+    }
+
+  render() {
+    const {showModal} = this.state;
+    const {toggleModal} = this;
+    return (
+      <>
+        <button type="button" onClick={toggleModal}>Открыть модалку</button>
+        {showModal && <Modal onClose={toggleModal}/> }
+    </>  
+    )
+  }
 }
 
 export default App;
